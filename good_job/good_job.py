5c0b8c9b24e52a6983292a0ef2135e3349b11eb1
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-04-12 17:23:47 -0700

    add inspirational quotes

--- good_job/good_job.py
+++ good_job/good_job.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 pygame.mixer.init()
 
@@ -14,19 +15,28 @@ pygame.mixer.init()
 #I have a supportive network I swear
 #little animation maybve? star comes in on a swirly pattern?
 #Make desktop shortcut
-#Add inspirational quote dictionary
 
-import random
 start = True
 good_job = pygame.mixer.Sound("ST_Fanfare_WinBattle.wav")
-print("You did it! Good job!")
+
+
 quotes = ["The pain you feel today is the strength you feel tomorrow. For every challenge encountered there is an opportunity for growth",
           "It's hard to beat a person who never gives up. Our greatest glory is not in never failing, but in rising every time we fail.",
-          "With or without you, there will be champions. You want your name there? Work hard."]
+          "With or without you, there will be champions. You want your name there? Work hard.",
+          "You can either experience the pain of discipline or the pain of regret. The choice is yours.” —Unknown",
+          "People who wonder if the glass is half empty or full miss the point. The glass is refillable. —Unknown",
+          "We are what we repeatedly do. Excellence, then, is not an act, but a habit. ―Aristotle",
+          "If you hear a voice within you say, ‘You cannot paint,’ then by all means paint, and that voice will be silenced. ―Vincent Van Gogh",
+          "“The hard days are what make you stronger.” ―Aly Raisman",
+          "“Opportunity is missed by most people because it is dressed in overalls and looks like work.”",
 
-print("\n")
+          ]
+
+
 
 print(quotes[random.randint(0, len(quotes)-1)])
+print("\n")
+print("You did it! Good job!")
 
 
 
