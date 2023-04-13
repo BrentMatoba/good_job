39f05fb90062a1c6948d9b83e0c4047c56974882
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-04-12 17:11:10 -0700

    Add inspirational quote array

--- good_job/good_job.py
+++ good_job/good_job.py
@@ -3,6 +3,8 @@ pygame.init()
 pygame.mixer.init()
 
 #things to do
+#reduce volume
+#improve sound loop code
 #game window
 #load star
 #make text boxes in window
@@ -12,18 +14,24 @@ pygame.mixer.init()
 #I have a supportive network I swear
 #little animation maybve? star comes in on a swirly pattern?
 #Make desktop shortcut
-#reduce volume
-#improve sound loop code
+#Add inspirational quote dictionary
 
-
+import random
 start = True
 good_job = pygame.mixer.Sound("ST_Fanfare_WinBattle.wav")
-length = pygame.mixer.Sound.get_length(good_job)
+print("You did it! Good job!")
+quotes = ["The pain you feel today is the strength you feel tomorrow. For every challenge encountered there is an opportunity for growth",
+          "It's hard to beat a person who never gives up. Our greatest glory is not in never failing, but in rising every time we fail.",
+          "With or without you, there will be champions. You want your name there? Work hard."]
+
+print("\n")
+
+print(quotes[random.randint(0, len(quotes)-1)])
+
+
 
 while start == True:
-    good_job = pygame.mixer.Sound("ST_Fanfare_WinBattle.wav")
     good_job.play(0, 0, 0)
-    print("You did it!")
     input("")
     start = False
 
