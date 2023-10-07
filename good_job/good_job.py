7b8fa0f5bae3c326d7e0c867d0f0013399341d71
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-10-07 14:21:00 -0700

    Cleaned up code into modular functions.

--- good_job/good_job.py
+++ good_job/good_job.py
@@ -19,7 +19,7 @@ def numToType(number):
     elif number == 0:
         return "testing"
 
-def writeLog(datetime, type, description):
+def writeLog(log, datetime, type, description):
     log.write(datetime)
     log.write(", ")
     log.write(type)
@@ -31,47 +31,20 @@ def writeLog(datetime, type, description):
     print()
     print()
 
-#things to do
-#reduce volume
-#improve sound loop code
-#game window
-#load star
-#make text boxes in window
-#Change font/size
-#One textbox optional, can user prompt to say what they did
-#is this fun or sad lmfao
-#I have a supportive network I swear
-#little animation maybve? star comes in on a swirly pattern?
-#Can make an in-app pomodoro. Create a timer somewhere that update each second. When it goes to zero have the rest of program automatically run.
-#Make desktop shortcut
-#Make pomodoro counter that persists between program runs by writing to file. Pseudo written
-#write main function
-#PyQT module is used to make gui with Python
-#make calculating point types function
-#make numbers for input, 1 = school, 2 = extracurricular etc, shoudl reduce capacity for user input error
-#make desktop shortcut to good_job
-#Wind rustling sound after good_job sound
-#Write function that can search csv file and delete testing rows
+def goodJobSound():
+    good_job = pygame.mixer.Sound("ST_Fanfare_WinBattle.wav")
+    good_job.play()
+    pygame.time.wait(int(good_job.get_length() * 1000))
 
-
-
-
-#log management
-with open('log.csv', 'a+') as log:
-    #gathers data for log entry
-    datetime = str(datetime.now())
-    num = input("What type of pomodoro? (1/2/3?)\n1:School\n2:Odin\n3:Extracurricular programming\n4:School Programming\n")
-    type = numToType(int(num))
-    description = input("What did you do during the pomodoro? ")
-
-    #Adds new line to log.csv
-    writeLog(datetime, type, description)
-
-
-
-
-
-
+#Randomly prints inspirational quote
+def printQuote():
+    print(quotes[random.randint(0, len(quotes)-1)])
+    print("You did it! Good job!")
+    print()
+    print()
+    print()
+    print()
+    print()
 
 #Stores quotes
 quotes = ["The pain you feel today is the strength you feel tomorrow. For every challenge encountered there is an opportunity for growth",
@@ -85,22 +58,25 @@ quotes = ["The pain you feel today is the strength you feel tomorrow. For every 
           "“Opportunity is missed by most people because it is dressed in overalls and looks like work.”"
           ]
 
-#Randomly prints inspirational quote
-print(quotes[random.randint(0, len(quotes)-1)])
-print("You did it! Good job!")
-print()
-print()
-print()
-print()
-print()
+def logPomodoro():
+    #log management
+    with open('log.csv', 'a+') as log:
+        #gathers data for log entry
+        current_datetime = str(datetime.now())
+        num = input("What type of pomodoro? (1/2/3/4?)\n1:School\n2:Odin\n3:Extracurricular programming\n4:School Programming\n")
+        type = numToType(int(num))
+        description = input("What did you do during the pomodoro? ")
+
+        #Adds new line to log.csv
+        writeLog(log, current_datetime, type, description)
 
 
-#plays sound
-good_job = pygame.mixer.Sound("ST_Fanfare_WinBattle.wav")
-good_job.play()
-pygame.time.wait(int(good_job.get_length() * 1000))
+    #print inspirational quote
+    printQuote()
+    #plays sound
+    goodJobSound()
 
 
-
+logPomodoro()
 
 
