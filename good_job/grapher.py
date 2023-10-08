73ee889d4b979eca577cce2c5b385cbc14a9eba4
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-10-08 16:54:43 -0700

    add countplot functionality

--- good_job/grapher.py
+++ good_job/grapher.py
@@ -6,6 +6,20 @@ import pandas as pd
 
 #import data from csv
 log = pd.read_csv('log.csv')
-#make bar graph
-print(log)
 
+
+
+def countPlot():
+    #Creates countplot of total pomodoros
+    #Alters initial size of countplot
+    plt.figure(figsize=(10, 8))
+    #countplot shows how many times something occurs
+    sns.countplot(x="Type", data=log)
+
+
+    #Makes graph appear
+    plt.show()
+
+
+
+countPlot()
