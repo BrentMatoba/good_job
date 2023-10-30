9ddf2fab35455420b0ac998d2868608a23ab981f
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-10-29 22:16:39 -0700

    add buttons

--- good_job/test.py
+++ good_job/test.py
@@ -3,7 +3,19 @@ root = tk.Tk()
 myLabel1 = tk.Label(root, text="Hello World", fg="white")
 myLabel2 = tk.Label(root, text="Hello World", fg="white")
 
-myLabel1.grid(row = 0, column = 0)
-myLabel2.grid(row=1, column=0)
+#add buttons, notably this didn't work while the grid was active, seems like one or the other?
+myButton = tk.Button(root, text="click me")
+myButton.pack()
+
+#Gives far more control over positioning than pack(). Remember that Python is Object oriented, and that these items are objects, and can be manipulated as such.
+#myLabel1.grid(row = 0, column = 0)
+#myLabel2.grid(row=1, column=0)
+
+
+
+
+
+
+
 
 root.mainloop()
