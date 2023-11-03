dcbe556e7c7d7c3293565299450ca5ab20fe93cf
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-02 23:41:01 -0700

    buttons

--- good_job/gui.py
+++ good_job/gui.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Pomodoro Tracker")
+root.geometry("300x300")
+
+
+startButton = tk.Button(root, text="start pomodoro")
+trackButton = tk.Button(root, text="track pomodoro")
+
+startButton.pack()
+trackButton.pack()
 
 root.mainloop()
