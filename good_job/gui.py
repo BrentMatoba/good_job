70912eb4add56a3d1cb32063e0382d121ecfc1b3
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-03 23:33:34 -0700

    start button

--- good_job/gui.py
+++ good_job/gui.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 
+
+
 root = tk.Tk()
 root.title("Pomodoro Tracker")
-root.geometry("300x300")
+root.geometry("500x500")
 
 
-startButton = tk.Button(root, text="start pomodoro")
+
+def start():
+    ##import good_job here, why doesn't import work??
+
+startButton = tk.Button(root, text="start pomodoro", command=start())
 trackButton = tk.Button(root, text="track pomodoro")
 
 startButton.pack()
