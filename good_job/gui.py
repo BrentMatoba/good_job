ae4851390205a40879e748f7aca91a993ddc94b8
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-03 23:36:19 -0700

    add countplot button

--- good_job/gui.py
+++ good_job/gui.py
@@ -10,11 +10,22 @@ root.geometry("500x500")
 
 def start():
     ##import good_job here, why doesn't import work??
+    return
 
-startButton = tk.Button(root, text="start pomodoro", command=start())
-trackButton = tk.Button(root, text="track pomodoro")
+def track():
+    ##import good_job track functions here
+    return
+def countplot():
+    #grapher functions here
+    return
+
+startButton = tk.Button(root, text="start pomodoro", command=start)
+trackButton = tk.Button(root, text="track pomodoro", command=track)
+countplotButton = tk.Button(root, text="countplot pomodoros", command=countplot)
+
 
 startButton.pack()
 trackButton.pack()
+countplotButton.pack()
 
 root.mainloop()
