400df76bd3763ab80c16d2a57a43f1532c78cb62
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-11 15:46:13 -0800

    Actually fix placeholder grid issue

--- good_job/gui.py
+++ good_job/gui.py
@@ -39,6 +39,9 @@ buttonFrame.grid(row=3, column=5, padx=80)
 
 
 
+
+
+#buttons
 startButton = tk.Button(buttonFrame, height=5, width=20,text="start pomodoro", command=start)
 trackButton = tk.Button(buttonFrame, height=5, width=20, text="record pomodoro", command=record)
 countplotButton = tk.Button(buttonFrame, height=5, width=20, text="countplot pomodoros", command=countplot)
@@ -46,15 +49,20 @@ histplotButton = tk.Button(buttonFrame, height=5, width=20, text="histplot pomod
 
 
 #move using the sticky method
-placeholderFrame = tk.Frame(root) #In TKinter, having items in different containers makes it so that they don't physically interact
-placeholderFrame.grid(row=3, column=10, padx=30)
-placeholderLabel = tk.Label(placeholderFrame, text="", bg="blue", width=80, height=30)
-placeholderLabel.grid()
+placeholderFrame = tk.Frame(root, width=30, height=30) #In TKinter, having items in different containers makes it so that they don't physically interact
+placeholderFrame.grid(row=4, column=4)
+placeholderLabel = tk.Label(root, bg="blue", width=50, height=20)
+placeholderLabel.place(x=600, y=50)
 
 
-startButton.grid(row=1, column=0)
-trackButton.grid(row=2, column=0)
-countplotButton.grid(row=3, column=0)
-histplotButton.grid(row=4, column=0)
+#startButton.grid(row=1, column=0)
+#trackButton.grid(row=2, column=0)
+#countplotButton.grid(row=3, column=0)
+#histplotButton.grid(row=4, column=0)
+
+startButton.pack()
+trackButton.pack()
+countplotButton.pack()
+histplotButton.pack()
 
 root.mainloop()
