23220f07cbcedaa0aba5dd512281ea82c34f4903
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-13 21:32:15 -0800

    pseudocode leveling system

---
+++ good_job/levels.py
@@ -0,0 +1,13 @@
+#I want to make an experience/leveling system even though I'm not completely done with the main functionality pogchamp
+#xp equation:  XP threshold ==alogb(Level)+c
+    #logarithmic, meaning fast start and gradually slower gains as you level up
+    #where to place badges?
+    #100 xp per level? why or why not?
+
+    ###Assign an XP value to each Pomodoro session. For simplicity, let's say 1 Pomodoro = 1 XP.
+        #Cumulative XP required for each JLPT level:
+        #N5: 440 XP
+        #N4: 1100 XP (440 from N5 + 660 additional)
+        #N3: 1980 XP (1100 from N4 + 880 additional)
+        #N2: 3080 XP (1980 from N3 + 1100 additional)
+        #N1: 4400 XP (3080 from N2 + 1320 additional)
